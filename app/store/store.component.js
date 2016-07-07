8ebda4bf2c8e453570bc202b500236f45d797086
Kostuch6 <lukas.marchel@gmail.com>
2016-07-07 13:00:37 +0200

    totally working

--- app/store/store.component.js
+++ app/store/store.component.js
@@ -15,12 +15,7 @@ angular.
                 this.upgrades = Upgrades.query();
                 var temp = { };
                 
-                this.buyBuilding = function(building) {
-                    building.number += 1;
-                    console.log("GPC, GPS, price", building.gpc, building.gps, building.price);
-                    this.currentGold -= building.price;
-                    $rootScope.$broadcast('income-upgraded', building);
-                    building.price = building.price + building.costUp;
+                this.check = function() {
                     for(i = 0; i < this.buildings.length; i++)
                     {
                         if(this.buildings[i].price <= this.currentGold)
@@ -34,54 +29,50 @@ angular.
                             console.log("Zmienilo na true");
                         }
                     }
-                    //stats = {price: building.price, gpcUP: building.gpc, gpsUP: building.gps};
-                    //console.log("GPC, GPS, price", stats.gpc, stats.gps, stats.price);
-                };
-                
-                this.buyUpgrade = function(upgrade) {
-                    upgrade.isUpgraded = true;
-                    console.log("upgrade", upgrade.isUpgraded);
-                    this.currentGold -= upgrade.price;
-                    console.log("numer", this.buildings[upgrade.building].number);
-                    console.log("gpc", this.buildings[upgrade.building].gpc);
-                    console.log("upgrade gpc", upgrade.gpcMod);
-                    var gpc = (this.buildings[upgrade.building].number * this.buildings[upgrade.building].gpc) * (upgrade.gpcMod - 1);
-                    console.log("gpcmod", gpc);
-                    var gps = (this.buildings[upgrade.building].number * this.buildings[upgrade.building].gps) * (upgrade.gpsMod - 1);
-                    console.log("gpsmod", gps);
-                    temp = { gpc : gpc, price : upgrade.price, gps : gps };
-                    console.log("chujnia", temp);
-                    $rootScope.$broadcast('income-upgraded', temp);
-                    this.buildings[upgrade.building].gpc *= upgrade.gpcMod;
-                    this.buildings[upgrade.building].gps *= upgrade.gpsMod;
-                };
-            
-                   
-                (function (param) {
-                    return $scope.$on('current-gold-changed', function(event, args) {
-                    param.currentGold = args.currentGold;
-                    for(i = 0; i < param.buildings.length; i++)
+                    
+                    for(i = 0; i < this.upgrades.length; i++)
                     {
-                        if(param.buildings[i].price <= param.currentGold)
+                        if(this.upgrades[i].price <= this.currentGold)
                         {
-                            param.buildings[i].isDisabled = false;
+                            this.upgrades[i].isDisabled = false;
                             console.log("Zmienilo na false");
                         }
                         else
                         {
-                            param.buildings[i].isDisabled = true;
+                            this.upgrades[i].isDisabled = true;
                             console.log("Zmienilo na true");
                         }
                     }
+                };
+                
+                this.buyBuilding = function(building) {
+                    building.number += 1;
+                    console.log("GPC, GPS, price", building.gpc, building.gps, building.price);
+                    this.currentGold -= building.price;
+                    $rootScope.$broadcast('income-upgraded', building);
+                    building.price = building.price + building.costUp;
+                    this.check();
+                };
+                
+                this.buyUpgrade = function(upgrade) {
+                    upgrade.isUpgraded = true;
+                    this.currentGold -= upgrade.price;
+                    this.check();
+                    var gpc = (this.buildings[upgrade.building].number * this.buildings[upgrade.building].gpc) * (upgrade.gpcMod - 1);
+                    var gps = (this.buildings[upgrade.building].number * this.buildings[upgrade.building].gps) * (upgrade.gpsMod - 1);
+                    temp = { gpc : gpc, price : upgrade.price, gps : gps };
+                    $rootScope.$broadcast('income-upgraded', temp);
+                    this.buildings[upgrade.building].gpc *= upgrade.gpcMod;
+                    this.buildings[upgrade.building].gps *= upgrade.gpsMod;
+                };
+             
+                (function (param) {
+                    return $scope.$on('current-gold-changed', function(event, args) {
+                    param.currentGold = args.currentGold;
+                    param.check();
                     console.log("currGold", param.currentGold);
                 });
                 })(this);
-//                $scope.$on('current-gold-changed', function(event, args) {
-//                    this.currentGold = args.currentGold;
-//                    console.log("currGold", this.currentGold);
-//                });
-               
-               
            }
         ]
 });
